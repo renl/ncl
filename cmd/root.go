486bc27bb9cb482f65be056786a9cc25a21d99c2
Ng Cheng Leong <renlore@hotmail.com>
2025-09-05 17:30:26 +0800

    refactor: remove unused toggle flag from root command

--- cmd/root.go
+++ cmd/root.go
@@ -40,8 +40,4 @@ func init() {
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ncl.yaml)")
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
